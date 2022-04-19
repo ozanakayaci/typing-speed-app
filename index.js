be9855a0ea554e0e9aea9571722914a15ba18969
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2022-04-19 23:33:13 +0300

    redux store,slice

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom/client";
+
 import App from "./src/App";
 
-reactDom.render(<App />, document.getElementById("root"));
+//redux
+import { store } from "./src/redux/store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/redux/typingslice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const typingSlice = createSlice({
+  name: "typing",
+  initialState: { paras: 4 },
+
+  reducers: {},
+});
+
+export default typingSlice.reducer;
