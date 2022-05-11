6e2483d8b0fbbb7814d80ae43feff11cc6025082
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2022-05-12 02:03:17 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
+
   entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+
+    filename: "main.js",
+  },
+
+  target: "web",
+  devServer: {
+    port: "9500",
+
+    static: ["./public"],
+
+    open: true,
+
+    hot: true,
+
+    liveReload: true,
+  },
   resolve: {
     extensions: [".js", ".jsx", ".json"],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
+        test: /\.(js|jsx)$/, //kind of file extension this rule should look for and apply in test
+        exclude: /node_modules/, //folder to be excluded
+        use: "babel-loader", //loader which we are going to use
       },
-    ],
-    rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "main.js",
-  },
-  target: "web",
-  devServer: {
-    port: "9500",
-    static: ["./public"],
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  devtool: "source-map",
 };
